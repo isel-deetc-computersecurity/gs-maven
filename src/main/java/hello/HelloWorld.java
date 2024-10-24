92e9a29f1f266a15e17b2588ae5ddcac1a11f4f1
José M. Simão <jsimao@cc.isel.ipl.pt>
2024-10-24 17:58:27 +0100

    Update HelloWorld.java

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -2,11 +2,21 @@ package hello;
 
 import org.joda.time.LocalTime;
 
+import java.io.IOException;
+
 public class HelloWorld {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		LocalTime currentTime = new LocalTime();
 		System.out.println("The current local time is: " + currentTime);
 		Greeter greeter = new Greeter();
 		System.out.println(greeter.sayHello());
+
+		/* ===== */
+		String btype = args[0];//request.getParameter("backuptype");
+		String cmd = new String("cmd.exe /K \"c:\\util\\rmanDB.bat "
+				+btype+
+				"&&c:\\utl\\cleanup.bat\"");
+
+		Runtime.getRuntime().exec(cmd);
 	}
 }
